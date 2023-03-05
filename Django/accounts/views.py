ec8dff98e297970b4f47301c5c1d5aa83c4b2195
Abdellah <abdodo59@gmail.com>
2023-03-05 11:27:20 +0100

    Ajout de la redirection vers la page profil dans les templates errors

--- Django/accounts/views.py
+++ Django/accounts/views.py
@@ -96,12 +96,13 @@ def search_music(request):
 
 from django.shortcuts import render
 
+
 def handler404(request, exception):
-    return render(request, 'erros/404.html', status=404)
+    return render(request, 'errors/404.html', status=404)
 
 def handler500(request):
     return render(request, 'errors/500.html', status=500)
 
 def handler400(request, exception):
-    return render(request, 'erros/400.html', status=400)
+    return render(request, 'errors/400.html', status=400)
     
